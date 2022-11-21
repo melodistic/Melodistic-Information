ee47ea21072e781d468d2af2474f906cc0b17c59
GGolfz <wisarutgolf11@gmail.com>
2022-11-21 16:55:31 +0700

    remove: lazy loading for preview image

--- src/components/header/Preview.tsx
+++ src/components/header/Preview.tsx
@@ -3,7 +3,7 @@ import styles from "/src/styles/header/Preview.module.css";
 const Preview = () => {
   return (
     <div id="preview" className={styles.PreviewContainer}>
-      <img src="images/mobile.webp" alt="preview-mobile" loading="lazy" />
+      <img src="images/mobile.webp" alt="preview-mobile" />
     </div>
   );
 };
