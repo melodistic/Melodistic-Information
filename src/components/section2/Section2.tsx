52240c7774d02e39d598e72978799de7084b6746
GGolfz <wisarutgolf11@gmail.com>
2022-11-21 11:36:04 +0700

    change: dot style and refactor member :sparkles:

--- src/components/section2/Section2.tsx
+++ src/components/section2/Section2.tsx
@@ -1,55 +1,62 @@
 import styles from "/src/styles/section2/Section2.module.css";
 
 const Section2 = () => {
+  const renderNumber = (number: string) => {
+    return (
+      <div className={styles.NumberContainer}>
+        <div className={styles.dot}></div>
+        <div className={styles.Subheader}>{number}</div>
+      </div>
+    );
+  };
   return (
     <div className={styles.Container} id="how-do-we-classify-mood-of-music">
-      <div className={styles.Content}>  
+      <div className={styles.Content}>
         <div className="header">How Do We Classify Mood Of Music?</div>
         <div className={styles.Leftblock}>
-            <div className={styles.Block}>
-                <img src="images/01.png"/>
-                <div  className={styles.Subheader}>Collect Dataset</div>
-            </div>
+          <div className={styles.Block}>
+            {renderNumber("01.")}
+            <div className={styles.Subheader}>Collect Dataset</div>
+          </div>
         </div>
         <div>
-            The music was randomly collected from YouTube Music in 6 categories of mood.
-            The audio files are converted to spectrogram
-            images to use as input for the model.
+          The music was randomly collected from YouTube Music in 6 categories of
+          mood. The audio files are converted to spectrogram images to use as
+          input for the model.
         </div>
-        <img
-        className={styles.Spectrogram}
-          src="images/spectrogram.png"
-        />
+        <img className={styles.Spectrogram} src="images/spectrogram.png" />
         <div className={styles.Rightblock}>
-            <div className={styles.Block}>
-                <img src="images/02.png"/>
-                <div className={styles.Subheader}>Model Development</div>
-            </div>
+          <div className={styles.Block}>
+            {renderNumber("02.")}
+            <div className={styles.Subheader}>Model Development</div>
+          </div>
         </div>
-        <img className={styles.model}
-        src="images/model-development.png"
-      />
-      <div>The spectrogram image will pass into a convolutional neural network and fully 
-        connected network before ending with softmax layer to get 6 nodes represents percentage 
-        of each mood. However, there is the research suggested that positive mood music 
-        influences beneficial effects of exercise, two negative moods will be filtered 
-        out from the application.</div>
+        <img className={styles.model} src="images/model-development.png" />
+        <div>
+          The spectrogram image will pass into a convolutional neural network
+          and fully connected network before ending with softmax layer to get 6
+          nodes represents percentage of each mood. However, there is the
+          research suggested that positive mood music influences beneficial
+          effects of exercise, two negative moods will be filtered out from the
+          application.
+        </div>
         <div className={styles.Leftblock}>
-            <div className={styles.Block}>
-                <img src="images/03.png"/>
-                <div className={styles.Subheader}>Model Evaluation</div>
-            </div>
+          <div className={styles.Block}>
+            {renderNumber("03.")}
+            <div className={styles.Subheader}>Model Evaluation</div>
+          </div>
         </div>
-        <div>To achieve the best performance of the model, we experiment with 3 types 
-            of convolutional layers: MobileNetV2, InceptionV3, and CNN model with 
-            multiple rounds of hyperparameter tuning.</div>
-        <img className={styles.Spectrogram}
-          src="images/mobilenet.png"
-        />
-        <img className={styles.Spectrogram}
-          src="images/table-model.png"
-        />
-        <div>The best model to deploy in the real system is MobileNetV2 model (0.938).</div>
+        <div>
+          To achieve the best performance of the model, we experiment with 3
+          types of convolutional layers: MobileNetV2, InceptionV3, and CNN model
+          with multiple rounds of hyperparameter tuning.
+        </div>
+        <img className={styles.Spectrogram} src="images/mobilenet.png" />
+        <img className={styles.Spectrogram} src="images/table-model.png" />
+        <div>
+          The best model to deploy in the real system is MobileNetV2 model
+          (0.938).
+        </div>
       </div>
     </div>
   );
--- src/components/section6/Section6.tsx
+++ src/components/section6/Section6.tsx
@@ -1,6 +1,32 @@
 import styles from "/src/styles/section6/Section6.module.css";
 
+interface Member {
+  name: string;
+  lastname: string;
+  quote: string;
+  image: string;
+}
 const Section6 = () => {
+  const members: Member[] = [
+    {
+      image: "images/grape.png",
+      name: "Kittiyaporn",
+      lastname: "Takrudthieng",
+      quote: "You can't be a bad designer if you don't put your work on",
+    },
+    {
+      image: "images/op.png",
+      name: "Kaewket",
+      lastname: "Saelee",
+      quote: "Automation for Jira assigned this issue to you",
+    },
+    {
+      image: "images/gop.png",
+      name: "Wisarut",
+      lastname: "Kitticharoenphonngam",
+      quote: "Tests can't fail, if there aren't any tests",
+    },
+  ];
   return (
     <div className={styles.Container} id="about-us">
       <div className={styles.Gradientbottom}></div>
@@ -8,30 +34,20 @@ const Section6 = () => {
       <img src="images/melody.png" className={styles.Melody} alt="melody" />
       <div className={styles.Content}>
         <div className="header">About Us</div>
-        <div className={styles.Member}>
-          <img className={styles.Member} src="images/grape.png" />
-          <div className={styles.Name}>Kittiyaporn</div>
-          <div className={styles.Lastname}>Takrudthieng</div>
-          <div className={styles.Quote}>
-            You can’t be a bad designer if you don’t put your work on
-          </div>
-        </div>
-        <div className={styles.Member}>
-          <img className={styles.Member} src="images/op.png" />
-          <div className={styles.Name}>Kaewket</div>
-          <div className={styles.Lastname}>Saelee</div>
-          <div className={styles.Quote}>
-            Automation for Jira assigned this issue to you
-          </div>
-        </div>
-        <div className={styles.Member}>
-          <img className={styles.Member} src="images/gop.png" />
-          <div className={styles.Name}>Wisarut </div>
-          <div className={styles.Lastname}>Kitticharoenphonngam</div>
-          <div className={styles.Quote}>
-            Tests can’t fail, if there aren’t any tests
-          </div>
-        </div>
+        {members.map((member, index) => {
+          return (
+            <div className={styles.Member} key={index}>
+              <img src={member.image} className={styles.Image} alt="member" />
+              <div className={styles.Name}>{member.name}</div>
+              <div className={styles.Lastname}>{member.lastname}</div>
+              <div className={styles.Quote}>
+                <div>“</div>
+                <div>{member.quote}</div>
+                <div>“</div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
