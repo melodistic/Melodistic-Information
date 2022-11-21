803266855e9654515453bfebd7ee243a352c0528
GGolfz <wisarutgolf11@gmail.com>
2022-11-21 15:03:49 +0700

    fix height of carousel

--- src/components/section2/AutoPlay.tsx
+++ src/components/section2/AutoPlay.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
-import '@splidejs/splide/css/skyblue';
-import '/src/styles/section2/Autoplay.css'
+import React from "react";
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
+import "@splidejs/splide/css/skyblue";
+import "/src/styles/section2/Autoplay.css";
 
 interface ConfusionMatrixData {
-    image: string;
-    label: string;
-  }
+  image: string;
+  label: string;
+}
 export const AutoplayExample = () => {
   const options = {
-    type         : 'loop',
-    gap          : '5rem',
-    autoplay     : true,
-    pauseOnHover : false,
+    type: "loop",
+    gap: "5rem",
+    autoplay: false,
+    pauseOnHover: false,
     resetProgress: false,
+    interval: 5000,
+    height: 'calc(80vw / 1.24 + 36px)'
   };
 
   const confusionList: Array<ConfusionMatrixData> = [
@@ -34,21 +36,20 @@ export const AutoplayExample = () => {
   return (
     <div>
       <Splide
-        options={ options }
+        options={options}
         aria-labelledby="autoplay-example-heading"
-        hasTrack={ false }
+        hasTrack={false}
       >
-        <div style={ { position: 'relative' } }>
+        <div style={{ position: "relative" }}>
           <SplideTrack>
-            { confusionList.map( slide => (
-              <SplideSlide key={ slide.image }>
-                <img src={ slide.image } alt={ slide.label }/>
-                <div style={{textAlign: 'center'}}>{slide.label}</div>
+            {confusionList.map((slide) => (
+              <SplideSlide key={slide.image}>
+                <img src={slide.image} alt={slide.label} loading="lazy" />
+                <div style={{ textAlign: "center" }}>{slide.label}</div>
               </SplideSlide>
-            ) ) }
+            ))}
           </SplideTrack>
         </div>
-
       </Splide>
     </div>
   );
